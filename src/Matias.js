747d829c9ade7051bd5afbd0468d98003fa34d05
Matsuuu <huhta.matias@gmail.com>
2017-04-13 22:45:29 +0300

    Matiaksen musamankeli toimii

--- src/Matias.js
+++ src/Matias.js
@@ -1,40 +1,74 @@
 import React, { Component } from 'react';
-import YouTube from './YouTube';
+import ReactDOM from 'react-dom';
+import * as Playlists from './playlists';
+import Youtube from 'react-youtube';
 
 export default class Content extends Component {
   render() {
-    /*const opts = {
-      height: '390',
-      width: '640',
-      playerVars: { // https://developers.google.com/youtube/player_parameters
-        autoplay: 1
-      }
-    };
-
-    return (
-      <YouTube
-        videoId="cG7cRDcPY3k"
-        opts={opts}
-        onReady={this._onReady}
-      />
-    );
-  }
-
-  _onReady(event) {
-    // access to player in all event handlers via event.target
-    //event.target.pauseVideo();
-  */
     return (
       <div>
-        <iframe src="https://www.youtube.com/playlist?list=PL9BCA60EEB1C8893D"></iframe>
+        <input type="checkbox" onClick={togglePlaylist.bind("dubstep")} name="playlists" id="dubstep" value="Dubstep"/>Dubstep <br />
+        <input type="checkbox" onClick={togglePlaylist.bind("dnb")} name="playlists" id="dnb" value="Dnb"/>DnB <br />
+        <input type="checkbox" onClick={togglePlaylist.bind("futurebass")} name="playlists" id="futurebass" value="Futurebass"/>Future bass <br />
       </div>   
     );
   }
 }
 
-const dnbPlaylist = 'https://www.youtube.com/watch?v=cG7cRDcPY3k&list=RDQMiuO43eLdcfI';
-const drumAndBass = getLinks(dnbPlaylist);
 
-function getLinks(pl) {
-    
+function togglePlaylist() {
+  /*var filepath = "playlists/dubstep.txt";
+  var file = new XMLHttpRequest();
+  file.open("GET", filepath, true);
+  file.onreadystatechange = function () {
+    if (file.readyState === 4) {
+      if (file.status === 200 || file.status == 0) {
+        var allText = file.responseText;
+        document.getElementById('content').innerHTML = allText;
+        alert(allText);
+      }
+    }
+  }
+  file.send(null);*/
+  // Tämä koodi toimisi, jos käytettäisiin esim pelkkää jquerya tai ihan vain vanilla javascriptiä, mutta reactin reititysprotokollat tuhoavat tämän toimivuuden.
+  //  Tästä syystä joudun tekemään tämän hieman eri lähestymistavalla.
+  
+  var checkboxes = document.getElementsByName('playlists');
+  var checked = [];
+  var total = 0;
+
+  for(var i = 0; i < checkboxes.length; i++) {
+    if(checkboxes[i].checked) {
+      var listname = checkboxes[i].value;
+      var call = eval('Playlists.get' + listname + '()');
+      call.forEach(function(song) {
+        checked.push(song);
+      });
+      total = total + call.length;
+    }
+  }
+
+  var playlist = checked;
+  var current = randomizeSong(playlist, total);
+
+  const opts = {
+    playerVars: {
+      autoplay: 1
+    }
+  };
+
+  ReactDOM.render(
+    <Youtube 
+    videoId={current}
+    onEnd={togglePlaylist}
+    opts={opts}
+    />,
+    document.getElementById('content2')
+  );
+}
+
+function randomizeSong(pl, total) {
+  var randint = Math.floor((Math.random() * total) + 1);
+  var current = pl[randint];
+  return current;
 }
